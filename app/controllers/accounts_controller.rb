a67b459e528878f4e082e0f7fc3ba682f75bff80
Chyld Medford <chyld.medford@gmail.com>
2017-08-10 23:16:49 +0000

    wip

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,4 +2,19 @@ class AccountsController < ApplicationController
     def index
         @accounts = Account.order(id: :desc)
     end
+
+    def atm
+        @account = Account.find(params[:id])
+    end
+
+    def deposit
+        @account = Account.find(params[:id])
+        @account.deposit(params[:amount].to_f)
+
+        if @account.errors.any?
+            render 'atm'
+        else
+            redirect_to root_url
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
     root to: 'accounts#index'
-    resources :accounts, only: [:index]
+    resources :accounts, only: [:index] do
+        member do
+            get '/atm', to: 'accounts#atm'
+            post '/deposit', to: 'accounts#deposit'
+        end
+    end
 end
